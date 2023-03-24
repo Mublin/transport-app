d06144c609e49fa38b86841267508623f6a8c701
[mubleen] <102492190+Mublin@users.noreply.github.com>
2023-03-24 17:46:07 +0100

    fixing some bugs

--- src/screens/BookingScreen.tsx
+++ src/screens/BookingScreen.tsx
@@ -2,21 +2,26 @@ import { PaystackButton } from 'react-paystack'
 import { useParams, Navigate } from "react-router-dom";
 import { data } from "../data";
 import { callback } from 'react-paystack/dist/types';
+import { useState, ChangeEvent } from 'react';
+import Form from "react-bootstrap/Form"
 
 export default function BookingScreen() {
+    const [name, setName] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+
     const {id, journey} = useParams();
     const route = data.journeys.find(drive=> drive.Route === journey)
     const rides = route?.available.find(ride => ride._id === Number(id))
     const config ={
         reference: (new Date()).getTime().toString(),
-        email: "",
+        email: email,
         amount: rides ? rides.Price * 100 : 0,
         publicKey: "pk_test_bdae6e992538ff698b264317e823fc9274bd793c"
       }
       const handlePaystackCloseAction = () => {
         // implementation for  whatever you want to do when the Paystack dialog closed.
         alert("closed")
-        console.log('closed')
+        // console.log('closed')
       }
       
       
@@ -41,7 +46,18 @@ export default function BookingScreen() {
             <p>Take-off Time: {rides.TakeOff}</p>
             <p>Description: {rides.description}</p>
         </div>) : <p>No journey available</p> : <Navigate to="/search" />}
-        {route ? rides ? <PaystackButton {...componentProps as { 
+        {route ? rides ? <Form style={{ maxWidth: "85dvw", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+            <h2>Passenger Information</h2>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" required minLength={5} onChange={(e: ChangeEvent<HTMLInputElement>)=> setName(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" required onChange={(e: ChangeEvent<HTMLInputElement>)=> setEmail(e.target.value)} />
+      </Form.Group>
+      {email.length > 5 && name.length > 5 &&  <PaystackButton {...componentProps as { 
             text: string;
             onSuccess: callback;
             onClose: () => void;
@@ -49,7 +65,8 @@ export default function BookingScreen() {
             email: string;
             amount: number;
             publicKey: string;
-        }} /> : <></> : <></> }
+        }} />}
+    </Form> : <></> : <></> } 
     </div>
   )
 }
